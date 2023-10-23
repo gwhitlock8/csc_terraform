d12671c1613b7bedc4db1de9e69dab30574d3886
Gavin Whitlock <gavinswhitlock@gmail.com>
2023-10-23 17:19:34 -0500

    first commit

---
+++ main.tf
@@ -0,0 +1,140 @@
+locals {
+    subnets = {
+        for x in var.subnets => {
+            subnet_name = x.subnet_name
+            subnet_ip = x.subnet_ip
+            subnet_region = x.subnet_region
+            subnet_description = x.subnet_description
+            subnet_purpose = x.subnet_purpose
+        }
+    }
+}
+locals {
+    routes = {
+        for x in var.routes => {
+            route_name = x.route_name
+            dest_range = x.dest_range
+            next_hop_ip = x.next_hop_ip
+            priority = x.priority
+        }
+    }
+}
+
+locals {
+    fw_rules = {
+        for x in var.firewall_rules => {
+            name = x.fw_name
+            description = x.fw_description
+            priority = x.priority
+            direction = x.direction
+            destination_ranges = x.destination_ranges
+            source_ranges = x.source_ranges
+            source_tags = x.source_tags
+            target_tags = x.target_tags
+            allow = for y in x.allow => {
+                protocol = y.protocol
+                ports = y.ports
+            }
+            deny = for z in x.deny => {
+                protocol = z.protocol
+                ports = z.ports
+            }
+
+        }
+    }
+}
+
+locals {
+    compute_instances = {
+        for x in var.compute_instances => {
+            name = x.instance_name
+            machine_type = x.machine_type
+            tags = [x.tags]
+            image = x.image
+        }
+    }
+}
+
+
+resource "google_compute_network" "vpc_network" {
+  project = var.project_id
+  name = var.network_name
+  auto_create_subnetworks = var.auto_create_subnetworks
+  mtu = var.mtu
+  description = var.network_description
+
+}
+
+resource "google_compute_subnetwork" "subnet" {
+    for_each = local.subnets
+
+    name = each.value.subnet_name
+    ip_cidr_range = each.value.subnet_ip
+    network = google_compute_network.vpc_network.self_link
+    region = each.value.subnet_region
+    description = each.value.subnet_description
+    purpose = each.value.subnet_purpose
+}
+
+resource "google_compute_route" "route" {
+    for_each = local.routes
+
+    name = each.value.route_name
+    dest_range = lookup(each.value, "destination_range", null)
+    network = google_compute_network.vpc_network.self_link
+    next_hop_ip = lookup(each.value, "next_hop_ip", null)
+    priority = each.value.priority
+}
+
+resource "google_compute_firewall" "firewall_rule" {
+    for_each = local.fw_rules
+
+    name = each.value.name
+    description = each.value.description
+    network = google_compute_network.vpc_network.self_link
+    priority = each.value.priority
+    direction = each.value.direction
+    destination_ranges = lookup(each.value, "destination_ranges", null)
+    source_ranges = lookup(each.value, "source_ranges", null)
+    source_tags = each.value.source_tags
+    target_tags = each.value.target_tags
+    
+    dynamic "allow" {
+        for_each = lookup(each.value, "allow", [])
+        content {
+            protocol = allow.value.protocol
+            ports = lookup(allow.value, "ports", null)
+        }
+    }
+
+    dynamic "deny" {
+        for_each = lookup(each.value,"deny", [])
+        content {
+            protocol = deny.value.protocol
+            ports    = lookup(deny.value, "ports", null)
+    }
+  }
+}
+
+resource "google_compute_instance" "default" {
+    for_each = local.compute_instances
+
+    name = each.value.name
+    machine_type = each.value.machine_type
+    tags = lookup(each.value,"tags", [])
+
+    boot_disk {
+        initialize_params {
+            image = each.value.image
+        }
+    }
+
+    network_interface {
+        network = google_compute_network.vpc_network.self_link
+    }
+}
+
+
+
+
+
---
+++ variables.tf
@@ -0,0 +1,99 @@
+//-------------NETWORK-----------------//
+
+variable "project_id" {
+  description = "The ID of the project where this VPC will be created"
+  type        = string
+}
+
+variable "region" {}
+
+variable "zone" {}
+
+variable "network_name" {
+  description = "The name of the network being created"
+  type        = string
+}
+
+variable "network_description" {
+  type        = string
+  description = "An optional description of this resource. The resource must be recreated to modify this field."
+  default     = ""
+}
+
+variable "auto_create_subnetworks" {
+  type        = bool
+  description = "When set to true, the network is created in 'auto subnet mode' and it will create a subnet for each region automatically across the 10.128.0.0/9 address range. When set to false, the network is created in 'custom subnet mode' so the user can explicitly connect subnetwork resources."
+  default     = false
+}
+
+
+variable "mtu" {
+  type        = number
+  description = "The network MTU (If set to 0, meaning MTU is unset - defaults to '1460'). Recommended values: 1460 (default for historic reasons), 1500 (Internet default), or 8896 (for Jumbo packets). Allowed are all values in the range 1300 to 8896, inclusively."
+  default     = 0
+}
+
+//--------------SUBNETS----------------//
+
+variable "subnets" {
+  type = list(object({
+    subnet_name                      = string
+    subnet_ip                        = string
+    subnet_region                    = string
+    subnet_description               = optional(string)
+    subnet_purpose                   = optional(string)
+
+  }))
+  description = "The list of subnets being created"
+}
+
+//--------------ROUTES----------------//
+
+variable "routes" {
+    type = list(object({
+        route_name = string
+        dest_range = string
+        route_network = string
+        next_hop_ip = string
+        priority = number
+    }))
+}
+
+
+//--------------FW RULES----------------//
+
+variable "firewall_rules" {
+    description = "List of ingress rules. This will be ignored if variable 'rules' is non-empty"
+    default     = []
+    type = list(object({
+        fw_name = string
+        fw_description = optional(string, null)
+        priority = optional(number, null)
+        direction = string
+        destination_ranges = optional(list(string), [])
+        source_ranges = optional(list(string), [])
+        source_tags = optional(list(string))
+        target_tags = optional(list(string))
+
+        allow = optional(list(object({
+            protocol = string
+            ports = optional(list(string))
+        })), [])
+        deny = optional(list(object({
+            protocol = string
+            ports    = optional(list(string))
+        })), [])
+    }))
+}
+
+//--------------COMPUTE INSTANCE----------------//
+
+variable "compute_instances" {
+    type = list(object({
+        instance_name = string
+        machine_type = string
+        instance_zone = string
+        tags = list(string)
+        image = string
+    }))
+}
